f087898923fb483470def121ff6c86158ea7407d
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-05 16:01:31 -0300

    agregar producto al carrito con localStorage

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,13 +1,43 @@
-window.addEventListener("load", function () {
+function productosEnElcarrito() {
+  return localStorage.carrito ? JSON.parse(localStorage.carrito).length : 0
+}
+
+window.addEventListener('load', function () {
   /*  Animations initialization */
-  new WOW().init();
+  new WOW().init()
 
   /* Toastr Initialization */
   toastr.options = {
-    positionClass: "toast-bottom-right",
+    positionClass: 'toast-bottom-right',
     fadeIn: 300,
     fadeOut: 1000,
     timeOut: 5000,
     extendedTimeOut: 1000,
-  };
-});
+  }
+
+  let botonesComprar = document.querySelectorAll('.agregar_carrito')
+  let cartNumber = document.querySelector('.cart-number')
+
+  cartNumber.innerText = productosEnElcarrito()
+
+  botonesComprar.forEach((boton) => {
+    boton.addEventListener('click', (e) => {
+      if (localStorage.getItem('carrito')) {
+        let carrito = JSON.parse(localStorage.getItem('carrito'))
+        let index = carrito.findIndex((prod) => prod.id == e.target.dataset.id)
+        if (index != -1) {
+          carrito[index].quantity++
+        } else {
+          carrito.push({ id: e.target.dataset.id, quantity: 1 })
+        }
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+      } else {
+        localStorage.setItem(
+          'carrito',
+          JSON.stringify([{ id: e.target.dataset.id, quantity: 1 }])
+        )
+      }
+      toastr.success('Se agregó este producto al carrito')
+    })
+  })
+})
