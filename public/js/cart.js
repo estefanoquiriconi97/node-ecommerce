9bacf6ae16964a22e43b9b68ec0b98e24d61469c
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-05 21:55:11 -0300

    Productos en el carrito y total

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,15 +3,59 @@ function setCarritoVacio() {
     <tr>     
         <td colspan="5"><div class="alert alert-warning my-2 text-center">No tienes products en el carrito</div></td>
     </tr>            
-    `;
+    `
 }
 function vaciarCarrito() {
-  localStorage.removeItem("carrito");
+  localStorage.removeItem('carrito')
 }
 
 function calcularTotal(products) {
   return products.reduce(
     (acum, product) => (acum += product.price * product.quantity),
     0
-  );
+  )
+}
+
+let cartRows = document.querySelector('.cartRows')
+
+const products = []
+
+if (localStorage.carrito) {
+  let carrito = JSON.parse(localStorage.carrito)
+  console.log(carrito)
+  carrito.forEach((item, index) => {
+    fetch(`/api/product/${item.id}`)
+      .then((res) => res.json())
+      .then((product) => {
+        if (product) {
+          cartRows.innerHTML += `
+          <tr id="row${index}">
+            <th scope="row">${index + 1}</th>
+              <td>${product.name}</td>
+              <td>$ ${product.price}</td>
+              <td class="text-center">${item.quantity}</td>
+              <td class="text-center">${parseFloat(
+                product.price * item.quantity,
+                2
+              ).toFixed(2)}</td>
+              <td><button class="btn btn-danger btn-sm" onclick="removeItem(${index})">X</button></td>
+          </tr>
+          `
+          products.push({
+            producId: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: item.quantity,
+          })
+        } else {
+          carrito.splice(index, 1)
+          localStorage.setItem('carrito', JSON.stringify(carrito))
+        }
+      })
+      .then(() => {
+        document.querySelector('.totalAmount').innerText = `$ ${calcularTotal(
+          products
+        )}`
+      })
+  })
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,8 @@ const controller = require("../controllers/homeController");
 const authMiddleware = require("../middlewares/authMiddleware");
 /* GET home page. */
 router.get("/", controller.home);
-router.get("/cart", authMiddleware, controller.cart);
+router.get("/cart", controller.cart);
+// router.get("/cart", authMiddleware, controller.cart);
 router.get("/order/:id", authMiddleware, controller.order);
 
 module.exports = router;
